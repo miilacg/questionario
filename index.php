c6a151ab228865bc0276122d64b941c99b010d8a
Camila <camiilacguimaraes@gmail.com>
2021-03-22 21:47:41 -0300

    LOGIN: implementação do formulário de login

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@500&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 
         <link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity = "sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin = "anonymous">
         <link rel = "stylesheet" href = "login.css">
@@ -56,14 +57,37 @@
             
         </form> -->
 
-        <div id="triangulos"></div>
-        <!-- <div id="brasao"></div> -->
+        <div id="triangulos">
+            <img src="./images/triangulos.png">
+        </div>
+
+        <!-- <div id="brasao">
+            <img src="./images/brasao.svg">
+        </div> -->
 
         <form method="POST" action="tratamentologin.php">            
             <main>
                 <img id="logo" src="./images/logo.svg" alt="logo scada-e">
-                <input id="botao" class="btn" type="submit"  name="entrar" value="Entrar"/>
+
+                <div class="formulario">                        
+                    <div class="form-group">
+                        <label>CPF</label>
+                        <input id="number" class="form-control" type="number" min="1" name="cpf" placeholder="Digite apenas os números" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Modalidade</label>
+                        <select id="curso" class="form-control" name="curso"  required>
+                            <option value="">Escolha a modalidade</option>
+                            <option value="Tecnico">Técnico</option> 
+                            <option value="Superior">Superior</option> 
+                            <option value="Administrador">Gerente</option>
+                        </select>
+                    </div> 
+                </div>
+                    
+                <input id="botao" class="btn" type="submit" name="entrar" value="Entrar"/>
             </main>           
-        </form>
+        </form>        
 	</body>
 </html>
